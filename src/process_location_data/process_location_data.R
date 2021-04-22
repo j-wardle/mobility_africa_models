5f8e503aa92e021a5615782043fd24910ceb27a4
Jack Wardle <j.wardle19@imperial.ac.uk>
2021-04-22 12:13:34 +0100

    port over existing code

---
+++ src/process_location_data/process_location_data.R
@@ -0,0 +1,189 @@
+## Load packages and function
+library(dplyr)
+library(tidyr)
+library(janitor)
+library(ggplot2)
+
+
+## Code to tidy existing dataset with population weighted centroids (data from 2015)
+## Update with 2006 estimates when we have them from Wes
+## Some manual cleaning so do for each country in turn
+
+## PORTUGAL
+
+adm2_populations <- read.delim("data/adm2-fixed.txt", encoding = 'UTF-8') %>% 
+  filter(ADM0 == "Portugal") %>% 
+  clean_names()
+
+id_names <- read.table("data/mobile_data/portugal/id_list.txt") %>% 
+  rename(id = V1,
+         name = V2)
+
+## Some municipalities have two entries with different population sizes
+## Some are on islands so we exclude, others have been labelled incorrectly (TAVIRA & GUIMARAES)
+
+adm2_populations <- adm2_populations %>%
+  mutate(adm2 = replace(adm2, adm2 == "Alcoutim" & pop == 25276, "Tavira"),
+         adm2 = replace(adm2, adm2 == "Braga" & pop == 158613, "Guimaraes")) %>% 
+  filter(!(adm2 == "Calheta")) %>%   # two municipalities on islands of Madeira and Azores
+  filter(!(adm2 == "Lagoa" & pop == 9263)) # remove municipality on Azores
+
+
+## Clean adm2 names to match municipalities in id_names, then filter to keep only matches
+
+adm2_populations$adm2 <- iconv(adm2_populations$adm2, from = "UTF-8", to = "ASCII//TRANSLIT")
+adm2_populations$adm2 <- gsub(" ", "_", toupper(adm2_populations$adm2))
+
+portugal_location_data <- adm2_populations %>% 
+  filter(adm2 %in% id_names$name) %>% 
+  select(adm2, pop, weighted_centroid_lon, weighted_centroid_lat) %>% 
+  rename(location = adm2,
+         population = pop,
+         x = weighted_centroid_lon,
+         y = weighted_centroid_lat) %>% 
+  arrange(location)
+
+# This file does not have the outlier population sizes that were in the data Moritz shared
+saveRDS(portugal_location_data, "data/portugal_location_data.rds")
+
+
+
+## Create a cleaned lookup for identifying the adm1 unit that each adm2 belongs in
+
+# First clean adm names in same way as before
+adm2_populations$adm1 <- iconv(adm2_populations$adm1, from = "UTF-8", to = "ASCII//TRANSLIT")
+adm2_populations$adm1 <- gsub(" ", "_", toupper(adm2_populations$adm1))
+
+adm_lookup <- adm2_populations %>% 
+  select(adm1, adm2)
+
+saveRDS(adm_lookup, "data/adm_lookup.rds")
+
+
+## Create cleaned pop and coordinates of adm1 units
+adm1_populations <- read.delim("data/adm1_centroids_fixed.tsv", encoding = 'UTF-8') %>% 
+  filter(ADM0 == "Portugal") %>% 
+  clean_names()
+
+adm1_populations$adm1 <- iconv(adm1_populations$adm1, from = "UTF-8", to = "ASCII//TRANSLIT")
+adm1_populations$adm1 <- gsub(" ", "_", toupper(adm1_populations$adm1))
+
+portugal_adm1_location_data <- adm1_populations %>% 
+  filter(!(adm1 %in% c("AZORES", "MADEIRA"))) %>% 
+  select(adm1, pop, centroid_lon, centroid_lat) %>% 
+  rename(location = adm1,
+         population = pop,
+         x = centroid_lon,
+         y = centroid_lat) %>% 
+  arrange(location)
+
+saveRDS(portugal_adm1_location_data, "data/portugal_adm1_location_data.rds")
+
+#########
+## FRANCE
+#########
+
+
+adm3_populations <- read.delim("data/france_pops.txt", encoding = 'UTF-8') 
+adm3_populations$pop <- as.numeric(gsub(",", "", adm3_populations$pop))
+
+id_names <- read.table("data/mobile_data/france/id_list.txt", encoding = 'UTF-8') %>% 
+  rename(id = V1,
+         name = V2)
+
+# Check for any duplicates
+unique(adm3_populations$adm3[duplicated(adm3_populations$adm3)])
+# there are two places called Saint-Denis and two called Saint-Pierre
+# Both Saint-Pierre's are on islands so can be deleted
+# Keep the larger Saint-Denis (second is on an island)
+
+adm3_populations <- adm3_populations %>%
+  filter(!(adm3 == "Saint-Pierre")) %>%   # remove two arrondissements on islands
+  filter(!(adm3 == "Saint-Denis" & pop == 204304)) # remove another arrondissement on island
+
+
+## Clean adm3 names to match names in id_names, then filter to keep only matches
+
+adm3_populations$adm3 <- iconv(adm3_populations$adm3, from = "UTF-8", to = "ASCII//TRANSLIT")
+adm3_populations$adm3 <- gsub(" ", "_", toupper(adm3_populations$adm3))
+
+id_names$name <- iconv(id_names$name, from = "UTF-8", to = "ASCII//TRANSLIT")
+id_names$name <- gsub(" ", "_", toupper(id_names$name))
+
+france_location_data <- adm3_populations %>% 
+  filter(adm3 %in% id_names$name) %>%    ##some boundaries have changed... how do we handle??
+  rename(location = adm3,
+         population = pop) %>% 
+  arrange(location)
+
+# File with France population data where adm matches
+saveRDS(france_location_data, "data/france_location_data.rds")
+
+
+## New data from Wes for 2007.
+# Clean here
+library(dplyr)
+library(janitor)
+
+adm3_populations <- read.delim("C:/Users/jw2519/Downloads/centroids_2007_3.txt") %>% 
+  filter(N0 == "France") %>% 
+  clean_names()
+
+adm3_populations$n3 <- iconv(adm3_populations$n3, to = "ASCII//TRANSLIT")
+adm3_populations$n3 <- gsub(" ", "_", toupper(adm3_populations$n3))
+
+compare <- read.delim("C:/Users/jw2519/Downloads/centroids_2007_3.txt") %>% 
+  filter(N0 == "France") %>% 
+  clean_names()
+
+
+id_names <- read.table("data/mobile_data/france/id_list.txt", encoding = 'UTF-8') %>% 
+  rename(id = V1,
+         name = V2)
+
+id_names$name <- iconv(id_names$name, from = "UTF-8", to = "ASCII//TRANSLIT")
+id_names$name <- gsub(" ", "_", toupper(id_names$name))
+
+pop_names <- adm3_populations$n3
+mob_names <- id_names$name
+
+
+adm3_populations <- adm3_populations %>% 
+  mutate(n3 = replace(n3, n2 == "Paris", "PARIS"),
+         n3 = replace(n3, n3 == "CHATEAU-CHINON_(VILLE)", "CHATEAU-CHINON(VILLE)"),
+         n3 = stringr::str_replace(n3, "LE_", "LE"),
+         n3 = stringr::str_replace(n3, "LA_", "LA"),
+         n3 = stringr::str_replace(n3, "LES_", "LES"))
+
+
+x <- adm3_populations %>% 
+  filter(n3 %in% id_names$name)
+
+y <- id_names %>% 
+  filter(!(name %in% adm3_populations$n3))
+
+z <- adm3_populations %>% 
+  filter(!(n3 %in% id_names$name)) %>% 
+  select(n3, pop)
+# 3 of the areas in the population dataset are not featured in the mobile data
+
+france_location_data_2007 <- adm3_populations %>% 
+  filter(n3 %in% id_names$name) %>%    
+  select(n3, pop, pop_cent_lon, pop_cent_lat) %>% 
+  rename(location = n3,
+         population = pop,
+         x = pop_cent_lon,
+         y = pop_cent_lat) %>% 
+  arrange(location)
+
+# each Paris arrondissement features so combine to give one Paris area (as in mobile data)
+france_location_data_2007 <- france_location_data_2007 %>% 
+  group_by(location) %>% 
+  summarise(x = weighted.mean(x, population),
+            y = weighted.mean(y, population),
+            population = sum(population)) %>% 
+  select(location, population, x, y)
+
+
+# File with France population data where adm matches
+saveRDS(france_location_data_2007, "data/france_location_data_2007.rds")
