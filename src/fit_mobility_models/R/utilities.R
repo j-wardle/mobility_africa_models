cad3dcdae13a257b45a174ca70b9d0107f3680a6
Jack Wardle <j.wardle19@imperial.ac.uk>
2021-04-27 19:35:46 +0100

    assign france pstay to portugal when fra gravity fit used to predict prtgl. generate matrix with numbers moving

--- src/fit_mobility_models/R/utilities.R
+++ src/fit_mobility_models/R/utilities.R
@@ -15,3 +15,32 @@ p_stay_average <- function(observed_movement) {
   out <- mean(out)
   
 }
+
+prob_predictions_to_numbers <- function(x) {
+  
+  imap(x, function(predictions, name) {
+    
+    if(isTRUE(startsWith(name, "france"))) {
+      
+      if(isTRUE(grepl("adm3", name))) {
+        location_data <- france_location_data
+      } else {
+        location_data <- france_adm2_location_data
+      }
+    }
+    
+    if(isTRUE(startsWith(name, "portugal"))) {
+      
+      if(isTRUE(grepl("adm2", name))) {
+        location_data <- portugal_location_data
+      } else {
+        location_data <- portugal_adm1_location_data
+      }
+    }
+    
+    conn_numbers <- predictions * location_data$population
+    conn_numbers <- as.movement_matrix(round(conn_numbers))
+    
+    conn_numbers
+  }) 
+}
