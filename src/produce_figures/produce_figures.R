d564355eba51e8ae0d32b7b7f7805b70fe33037a
Jack Wardle <j.wardle19@imperial.ac.uk>
2021-05-05 18:51:48 +0100

    cleaning messy format for figures

--- src/produce_figures/produce_figures.R
+++ src/produce_figures/produce_figures.R
@@ -1,10 +1,117 @@
-
 ## Create folder to save figures
 dir.create("figures")
 
-results2 <- tidyr::pivot_wider(results,
-                               id_cols = patch,
-                               names_from = model,
-                               values_from = `0.5`)
+## Load metric data
+collated_time_to_peak <- readRDS("collated_time_to_peak.rds")
+collated_time_to_first_case <- readRDS("collated_time_to_first_case.rds")
 
-results2$patch_name <- location_data$location
+## Load location data
+portugal_location_data <- readRDS("portugal_location_data.rds")
+portugal_adm1_location_data <- readRDS("portugal_adm1_location_data.rds")
+france_location_data<- readRDS("france_location_data.rds")
+france_adm2_location_data <- readRDS("france_adm2_location_data.rds")
+
+## Figure 1: time_to_peak with raw data
+
+# extract the country names and model type to be more clear
+# https://stackoverflow.com/questions/12297859/remove-all-text-before-colon
+
+collated_time_to_peak <- collated_time_to_peak %>% 
+  mutate(country = word(model, 1, sep ="\\_"),
+         model = regmatches(model,
+                            gregexpr("(?<=_).*", model, perl=TRUE)
+                            )
+         )
+
+collated_time_to_peak$model <- as.character(collated_time_to_peak$model)
+collated_time_to_peak <- collated_time_to_peak %>% 
+  arrange(country, model)
+
+collated_time_to_peak %>% 
+  group_by(country, model) %>% 
+  summarise(n = n()/2) # check number of entries per grouping
+
+# create a vector of the appropriate location names
+# TO DO: come up with a better, non-manual way of doing this
+location_names <- c(
+  rep(france_location_data$location, 2),
+  rep(france_aggr_location_data$location, 4),
+  rep(france_location_data$location, 4),
+  rep(france_aggr_location_data$location, 4),
+  rep(france_location_data$location, 4),
+  rep(france_aggr_location_data$location, 2),
+  rep(portugal_location_data$location, 2),
+  rep(portugal_aggr_location_data$location, 4),
+  rep(portugal_location_data$location, 4),
+  rep(portugal_aggr_location_data$location, 4),
+  rep(portugal_location_data$location, 4),
+  rep(portugal_aggr_location_data$location, 2)
+)
+
+collated_time_to_peak$patch_name <- location_names
+
+collated_time_to_peak <- collated_time_to_peak %>% 
+  mutate(seed = replace(seed, seed == "bre", "BREST"),
+         seed = replace(seed, seed == "prs", "PARIS"),
+         seed = replace(seed, seed == "mdd", "MIRANDA_DO_DOURO"),
+         seed = replace(seed, seed == "lsbn", "LISBOA"),
+         country = replace(country, country == "fra", "france"),
+         country = replace(country, country == "prtl", "portugal"),)
+
+
+## extract different model names
+model_types <- rep(unique(collated_time_to_peak$model), each = 4)
+countries <- rep(rep(unique(collated_time_to_peak$country), each = 2), 10)
+seed <- rep(unique(collated_time_to_peak$seed), 10)
+
+# messed up collating the results too early
+# re_split
+test <- pmap(list(model_types, countries, seed), function(m, c, s) {
+  
+  out <- filter(collated_time_to_peak, model == m & country == c & seed == s)
+  
+  if(isTRUE((grepl("aggr", m, fixed = TRUE)))) {
+    location_data <- readRDS(paste0(c, "_aggr_location_data.rds"))
+  } else {
+    location_data <- readRDS(paste0(c, "_location_data.rds"))
+  }
+  
+  
+  
+})
+
+
+results <- map(test, function(x) {
+  
+  
+  
+})
+
+
+# 
+# 
+# if(collated_time_to_peak$country == "fra") {
+#   
+#   if(isTRUE((grepl("aggr", collated_time_to_peak$model, fixed=TRUE)))) {
+#     
+#     collated_time_to_peak$patch_name <- test1
+#     
+#   } else {
+#     
+#     collated_time_to_peak$patch_name <- test2
+#     
+#   } 
+#   
+# } else if (collated_time_to_peak$country == "prtl") {
+#   
+#   if(isTRUE((grepl("aggr", collated_time_to_peak$model, fixed=TRUE)))) {
+#     
+#     collated_time_to_peak$patch_name <- test3
+#     
+#   } else {
+#     
+#     collated_time_to_peak$patch_name <- test4
+#     
+#   } 
+#   
+# }
