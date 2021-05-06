6d4bebdf73325eeb84b02b509143711ecd7e91a2
Jack Wardle <j.wardle19@imperial.ac.uk>
2021-05-06 16:04:43 +0100

    borders around facets

--- src/produce_figures/produce_figures.R
+++ src/produce_figures/produce_figures.R
@@ -116,7 +116,8 @@ peak_time_fig1 <-
   ylab("Time to peak (days)") +
   theme_classic() +
   facet_grid(seed ~ model,
-             labeller = labeller(model = model.labs))
+             labeller = labeller(model = model.labs)) +
+  theme(panel.border = element_rect(colour = "black", fill = NA))
 
 ggsave("figures/peak_time_fig1.pdf")
 
@@ -139,7 +140,8 @@ plots_adm_small <- map(adm_small_models, function(m) {
     ylab("peak from mobility model - peak from raw data (days)") +
     ggtitle(m) +
     theme_classic() +
-    facet_wrap(~seed, nrow = 1)
+    facet_wrap(~seed, nrow = 1) +
+    theme(panel.border = element_rect(colour = "black", fill = NA))
   
 })
 
@@ -162,7 +164,8 @@ plots_adm_big <- map(adm_big_models, function(m) {
     ylab("peak from mobility model - peak from raw data (days)") +
     ggtitle(m) +
     theme_classic() +
-    facet_wrap(~seed, nrow = 1)
+    facet_wrap(~seed, nrow = 1) +
+    theme(panel.border = element_rect(colour = "black", fill = NA))
   
 })
 
@@ -170,6 +173,37 @@ pdf("figures/big_models_peak_difference.pdf")
 plots_adm_big
 dev.off()
 
+
+
+## Now plot time with raw data (x-axis) against time with model (y-axis)
+
+plots_adm_small_scatter <- map(adm_small_models, function(m) {
+  
+  model_col <- as.character(m)
+  
+  results %>% 
+    filter(model == "raw" | model == m) %>%
+    pivot_wider(id_cols = c(patch, seed),
+                names_from = model,
+                values_from = median) %>% 
+    # group_by(country, patch) %>% 
+    # mutate(time_diff = median - median[model == "raw"]) %>%
+    # filter(model == m) %>% 
+    ggplot() +
+    geom_point(aes(x = raw, y = model_col), size = 0.9) +
+    geom_abline(yintercept = 0, colour = "red", linetype = 2) +
+    xlab("Time to peak with raw data (days)") +
+    ylab("Time to peak with mobility data (days)") +
+    ggtitle(m) +
+    theme_classic() +
+    facet_wrap(~seed, nrow = 1) +
+    theme(panel.border = element_rect(colour = "black", fill = NA))
+  
+})
+
+pdf("figures/small_models_peak_difference.pdf")
+plots_adm_small
+dev.off()
 
 
 
@@ -257,7 +291,8 @@ first_cases_fig1 <-
   ylab("Time to first case (days)") +
   theme_classic() +
   facet_grid(seed ~ model,
-             labeller = labeller(model = model.labs))
+             labeller = labeller(model = model.labs)) +
+  theme(panel.border = element_rect(colour = "black", fill = NA))
 
 ggsave("figures/first_case_fig1.pdf")
 
@@ -277,7 +312,8 @@ first_cases_adm_small <- map(adm_small_models, function(m) {
     ylab("first case from mobility model - first case from raw data (days)") +
     ggtitle(m) +
     theme_classic() +
-    facet_wrap(~seed, nrow = 1)
+    facet_wrap(~seed, nrow = 1) +
+    theme(panel.border = element_rect(colour = "black", fill = NA))
   
 })
 
@@ -288,7 +324,7 @@ dev.off()
 
 first_cases_adm_big <- map(adm_big_models, function(m) {
   
-  results %>% 
+  first_cases %>% 
     filter(model == "raw_aggr" | model == m) %>%
     group_by(country, patch) %>% 
     mutate(time_diff = median - median[model == "raw_aggr"]) %>%
@@ -300,7 +336,8 @@ first_cases_adm_big <- map(adm_big_models, function(m) {
     ylab("first case from mobility model - first case from raw data (days)") +
     ggtitle(m) +
     theme_classic() +
-    facet_wrap(~seed, nrow = 1)
+    facet_wrap(~seed, nrow = 1) +
+    theme(panel.border = element_rect(colour = "black", fill = NA))
   
 })
 
