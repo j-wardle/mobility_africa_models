c330948ae4b94b484735ff61d119cd576e119af0
Jack Wardle <j.wardle19@imperial.ac.uk>
2021-08-05 17:05:39 +0100

    join location coordinates, estimate distances

--- src/first_case_figures/first_case_figures.R
+++ src/first_case_figures/first_case_figures.R
@@ -9,8 +9,6 @@ time_to_first_case_path2 <- readRDS("collated_time_to_first_case_path2.rds")
 time_to_first_case <- bind_rows(time_to_first_case_path1, time_to_first_case_path2,
                                 .id = "pathogen")
 
-
-
 ## Load location data
 portugal_location_data <- readRDS("portugal_location_data.rds")
 france_location_data<- readRDS("france_location_data.rds")
@@ -31,8 +29,6 @@ time_to_first_case <- time_to_first_case %>%
 time_to_first_case$model <- as.character(time_to_first_case$model)
 time_to_first_case <- time_to_first_case %>% 
   arrange(country, model)
-
-time_to_first_case$patch_name <- location_names
 
 time_to_first_case <- time_to_first_case %>% 
   mutate(seed = replace(seed, seed == "bre", "BREST"),
@@ -63,49 +59,37 @@ location_names <- rep(france_location_data$location, 8)
 
 time_to_first_case$patch_name <- location_names
 
-first_cases <- time_to_first_case
 
+# Join location coordinates for patch and relevant seed location
 
-### TO DO: REVIEW AND IMPLEMENT THIS BIT
+france_location_data$country <- "FRANCE"
+portugal_location_data$country <- "PORTUGAL"
+location_data <- bind_rows(france_location_data, portugal_location_data)
 
-# ## extract different model names
-# model_types <- rep(unique(time_to_first_case$model), each = 4)
-# countries <- rep(rep(unique(time_to_first_case$country), each = 2), 10)
-# seed <- rep(unique(time_to_first_case$seed), 10)
-# 
-# # messed up by collating the results too early
-# # re_split data: TO DO - better way to do this
-# first_cases <- pmap_dfr(list(model_types, countries, seed), function(m, c, s) {
-#   
-#   out <- filter(collated_time_to_first_case, model == m & country == c & seed == s)
-#   central_point <- s
-#   
-#   if(isTRUE((grepl("aggr", m, fixed = TRUE)))) {
-#     location_data <- readRDS(paste0(c, "_aggr_location_data.rds"))
-#     
-#     if(s == "BREST") central_point <- "FINISTERE"
-#     if(s == "MIRANDA_DO_DOURO") central_point <- "BRAGANCA"
-#     
-#   } else {
-#     location_data <- readRDS(paste0(c, "_location_data.rds"))
-#   }
-#   
-#   location_data$seed_x <- location_data$x[location_data$location == central_point]
-#   location_data$seed_y <- location_data$y[location_data$location == central_point]
-#   
-#   location_data <- location_data %>%
-#     rowwise() %>% 
-#     mutate(distance = geosphere::distm(c(x, y), c(seed_x, seed_y)) / 1000
-#     )
-#   
-#   out$distance <- location_data$distance
-#   out
-#   
-# })
-# 
-# first_cases <- first_cases %>% 
-#   group_by(seed) %>% 
-#   mutate(scaled_distance = distance / max(distance))
+first_cases <- left_join(time_to_first_case, location_data,
+                         by = c("patch_name" = "location")) %>% 
+  select(-population) %>% 
+  rename(patch_x = x,
+         patch_y = y)
+
+first_cases <- left_join(first_cases, location_data,
+                         by = c("seed" = "location")) %>% 
+  select(-population) %>% 
+  rename(seed_x = x,
+         seed_y = y)
+
+## Caluclate distances from seed to patch
+
+first_cases <- first_cases %>% 
+  rowwise() %>%
+  mutate(distance = geosphere::distm(c(patch_x, patch_y), c(seed_x, seed_y)) / 1000
+  )
+
+first_cases <- first_cases %>%
+  group_by(seed) %>%
+  mutate(scaled_distance = distance / max(distance))
+
+## Tidy variable names
 
 first_cases <- rename(first_cases, median = `0.5`)
 first_cases$model <- as.factor(first_cases$model)
@@ -114,25 +98,18 @@ first_cases$`95%CrI` <- first_cases$`0.975` - first_cases$`0.025`
 first_cases$standardised_variation <- first_cases$`95%CrI` / first_cases$median
 
 
-
-
 ### Create scatter plot
+###x-axis : time to first case with observed mobility data
+### y-axis : time to first case with predicted mobility data
 
-### TO DO: Need to tidy this up
-### TO Do: at the moment, pathogen 1 and pathogen 2 are the same...
-
-a <- first_cases %>% 
-  pivot_wider(id_cols = c(patch, seed, model, pathogen),
-              names_from = model,
-              values_from = median)
-# first_cases_scatter <-
+first_cases_scatter <-
   first_cases %>% 
-    pivot_wider(id_cols = c(patch, seed, model, pathogen),
+    pivot_wider(id_cols = c(patch, seed, model, pathogen, scaled_distance),
                 names_from = model,
                 values_from = median) %>% 
     ggplot() +
-    geom_point(aes(x = raw, y = g2_alt), size = 0.8) +
-    # scale_colour_viridis_c(name = "Scaled\ndistance") +
+    geom_point(aes(x = raw, y = g2_alt, colour = scaled_distance), size = 0.8) +
+    scale_colour_viridis_c(name = "Scaled\ndistance") +
     geom_abline(slope = 1, intercept = 0, colour = "red", linetype = 2) +
     xlab("Time to first case using observed mobility (days)") +
     ylab("Time to first case using\npredicted mobility (days)") +
@@ -146,8 +123,6 @@ a <- first_cases %>%
     theme(panel.border = element_rect(colour = "black", fill = NA),
           axis.text = element_text(size = 7),
           plot.title = element_text(hjust = 0.5))
-  
 
-
-
-
+ggsave("figures/france_first_cases_scatter.png", first_cases_scatter,
+       width = 10, height = 8.65, units = "in")
