7f663dfb873e3cee41b55b0af06e1d5800875365
sangeeta <sangeetabhatia03@gmail.com>
2022-01-07 12:25:10 +0000

    Moving files from old repo

---
+++ src/produce_summary_maps/R/map_data_availability.R
@@ -0,0 +1,42 @@
+## data_features has at least the following columns:
+## long, lat, datasource_type, data_available and adm_level
+## adm_level is a string specified as ADM0, ADM1, ADM2, ADM3,
+## or, specific_location
+map_data_availability <- function(data_features) {
+
+    fname <- here::here("data/Africa_SHP")
+    africa <- sf::st_read(fname)
+
+    p <- ggplot() + theme_map() + xlab("") + ylab("")
+    p <- p + geom_sf(data = africa, lwd = 0.1, alpha = 0.3)
+    p <- p + coord_sf(datum = NA)
+    p <- p +
+        geom_point(data = data_features,
+                   aes(x = long,
+                       y = lat,
+                       col = datasource_type,
+                       alpha = data_available,
+                       size = scale
+                       ),
+                   position = "jitter",
+                   shape = 21 ## hollow circle; migh help see overlapping points.
+                   )
+
+    p <- p + scale_color_manual(
+                 values = datasource_palette,
+                 labels = datasource_labels,
+                 aesthetics = c("colour", "fill")
+             )
+
+    p <- p + scale_alpha_manual(
+                 values = availability_scale, guide = FALSE
+             )
+    p <- p + scale_size_manual(
+                 values = size_scale,
+                 guide = FALSE
+             )
+    p <- p + paper_theme$theme + paper_theme$legend
+
+    p
+
+}
---
+++ src/produce_summary_maps/R/utils.R
@@ -0,0 +1,58 @@
+get_centroid <- function(sf_obj, name_col) {
+
+    geoms <- sf::st_geometry(sf_obj)
+    centroids <- sf::st_centroid(geoms)
+    coords <- sf::st_coordinates(centroids)
+    out <- data.frame(
+        location = sf_obj[[name_col]],
+        country = unique(sf_obj[["GID_0"]]),
+        long = coords[, 1],
+        lat = coords[ , 2]
+    )
+    out
+}
+
+## Join data locations recorded in
+## data/estimated_data_locations_scale.csv and
+## data/empirical_data_locations_scale.csv with
+## and type availabilty recorded in data/study_ds_types.csv
+## category must be either empirical or estimated
+prepare_data_features <- function(data_locations,
+                                  features,
+                                  category = c("empirical", "estimated")) {
+
+    match.arg(category)
+
+    features <- features[features$data_category == category, ]
+    out <- dplyr::left_join(
+        data_locations, features, by = c("bibkey", "country")
+        )
+    out <- out[out$country %in% africa, ]
+
+    ## Rearrange factor levels so that legends come out OK.
+    out$datasource_type <- factor(
+        out$datasource_type,
+        levels = c(
+            "cdr", "ipums", "census", "interview", "hdss",
+            "GBMD", "unhcr", "genomic", "flight_capacity",
+            "data_other_countries", "incidence", "estimates_other",
+            "flowminder", "social_media"
+        ),
+        ordered = TRUE
+    )
+
+    out
+}
+
+
+normalised_flow_matrix <- function(pops, distances, params) {
+
+    out <- mover::flow_matrix(pops, distances, params)
+    out <- out / pops
+    diag(out) <- 1 - rowSums(out, na.rm = TRUE)
+
+    out
+
+
+
+}
